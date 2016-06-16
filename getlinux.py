d09c748ac9f9cee021074902d0bbeb4a98c1aea9
yangdong <yangdong@chaoyuehudong.com>
2016-06-16 16:49:39 +0800

    修改

--- getlinux.py
+++ getlinux.py
@@ -5,16 +5,13 @@ import re
 
 import urllib
 import requests
+import socket
+import os
+
+socket.setdefaulttimeout(30)
 
 
 def getHtml(url):
-    # data = urllib.urlencode(values)
-    # headers = {"User-agent": "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1"}
-    # req = urllib2.Request(url, data, headers)
-    # cj = cookielib.CookieJar()
-    # opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    # response = opener.open(req)
-    # html = response.read()
     response = urllib.urlopen(url)
     html = response.read()
     return html
@@ -30,30 +27,40 @@ def getDownloadUrls(url):
         if path == '/':
             continue
         if path.find('.pdf') > 0 or path.find('.rar') > 0 or path.find('.zip') > 0 or path.find('.tar.gz') > 0:
-            urls.append(path)
+            if len(path.split('/')[-1]) > 0:
+                try:
+                    tra_str = urllib.unquote(path.split('/')[-1]).decode('utf-8')
+                    local_filename = local_path + '/' + tra_str
+                    filename, msg = urllib.urlretrieve(golbalurl + path[1:], local_filename)
+                    print "local_filename", filename, msg
+                except Exception, e:
+                    urllib.urlcleanup()
+                    print "load file failed", e
+
             continue
         newurl = golbalurl + path[1:]
         urls += getDownloadUrls(newurl)
     return urls
 
 
-def downloadFile(url, index):
-    local_filename = str(index)+"-"+url.split('/')[-1]
-    # NOTE the stream=True parameter
-    r = requests.get(url, stream=True)
-    with open(local_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-                f.flush()
-    return local_filename
+# def downloadFile(url, index):
+#     local_filename = str(index)+"-"+url.split('/')[-1]
+#     # NOTE the stream=True parameter
+#     r = requests.get(url, stream=True)
+#     with open(local_filename, 'wb') as f:
+#         for chunk in r.iter_content(chunk_size=1024):
+#             if chunk:  # filter out keep-alive new chunks
+#                 f.write(chunk)
+#                 f.flush()
+#     return local_filename
 
+
+def iota():
+    n += 1
+    return n
 
 # data = {'username': 'www.linuxidc.com', 'passworld': 'www.linuxidc.com'}
 golbalurl = 'http://linux.linuxidc.com/'
+local_path = os.path.abspath('.')
+local_path += '/doc'
 pathlist = getDownloadUrls(golbalurl)
-n = 0
-for path in pathlist:
-    print "loadfile:", path.split('/')[-1]
-    downloadFile(golbalurl + path, n)
-    n += 1
